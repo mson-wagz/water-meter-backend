505ec079a8a12e7f12c70febeb1bc27b92148b1f
Abigail Wagura <abigailwagura1@gmail.com>
2025-07-14 15:58:07 +0300

    restructuring the backend

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 5000
 
 
 app.post("/login", async (req, res) => {
@@ -209,4 +209,4 @@ app.get("/api/payments/:meterReadingId", async (req, res) => {
   }
 })
 
-app.listen(PORT, () => console.log("API server running on port 3001"))
+app.listen(PORT, () => console.log("Server running on port 5000"))
